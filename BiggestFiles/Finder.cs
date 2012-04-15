4f7738d0a99eb9f27839b54f0edf9567908b8e88
Michel Bretschneider <embix@gmx.net>
2012-04-15 20:18:37 +0200

    Bugfix: handle exceptions on accessing files/directories

--- BiggestFiles/Finder.cs
+++ BiggestFiles/Finder.cs
@@ -17,9 +17,9 @@ namespace BiggestFiles
             _startingDirectory = new DirectoryInfo(startingPath);
         }
 
-        public IEnumerable<String> Find()
+        public IEnumerable<String> FindRecursivly()
         {
-            var biggestFiles = GetBiggestFilesInDirectory(_startingDirectory);
+            var biggestFiles = GetBiggestFilesInDirectoryRecursively(_startingDirectory);
             var resultLines = new List<String>();
             foreach (var file in biggestFiles)
             {
@@ -28,17 +28,58 @@ namespace BiggestFiles
             return resultLines;
         }
 
-        public IEnumerable<FileInfo> GetBiggestFilesInDirectory(DirectoryInfo directory)
+        public IEnumerable<FileInfo> GetBiggestFilesInDirectoryRecursively(DirectoryInfo directory)
         {
-            var biggestFilesInCurrentDirectory = directory.EnumerateFiles().OrderByDescending(file => file.Length).Take(_fileCount);
-            var directoriesInCurrentDirectory = directory.EnumerateDirectories();
+            IEnumerable<FileInfo> biggestFilesInCurrentDirectory = TryGetBiggestFilesInCurrentDirectory(directory);
+            IEnumerable<DirectoryInfo> directoriesInCurrentDirectory = TryGetDirectoriesInCurrentDirectory(directory);
             foreach (var subDirectory in directoriesInCurrentDirectory)
             {
-                var biggestFilesInSubDirectory = GetBiggestFilesInDirectory(subDirectory);
+                var biggestFilesInSubDirectory = GetBiggestFilesInDirectoryRecursively(subDirectory);
                 biggestFilesInCurrentDirectory = 
                     biggestFilesInCurrentDirectory.Union(biggestFilesInSubDirectory).OrderByDescending(file => file.Length).Take(_fileCount);
             }
             return biggestFilesInCurrentDirectory;
+        }
+
+        private IEnumerable<DirectoryInfo> TryGetDirectoriesInCurrentDirectory(DirectoryInfo directory)
+        {
+            try
+            {
+                return directory.EnumerateDirectories();
+            }
+            catch (UnauthorizedAccessException exception)
+            {
+                // TODO: Logging, Warning
+            }
+            catch(Exception exception)
+            {
+                // TODO: Logging, Strong Warning
+            }
+            return new DirectoryInfo[0];
+        }
+
+        private IEnumerable<FileInfo> TryGetBiggestFilesInCurrentDirectory(DirectoryInfo directory)
+        {
+            try
+            {
+                return GetBiggestFilesInCurrentDirectory(directory);
+            }
+            catch (UnauthorizedAccessException exception)
+            {
+                // TODO: Logging, Warning
+            }
+            catch(Exception exception)
+            {
+                // TODO: Logging, Strong Waring
+            }
+            return new FileInfo[0];
+        }
+
+        private IEnumerable<FileInfo> GetBiggestFilesInCurrentDirectory(DirectoryInfo directory)
+        {
+            var filesInDirectory = directory.EnumerateFiles();
+            var byDescending = filesInDirectory.OrderByDescending(file => file.Length);
+            return byDescending.Take(_fileCount);
         }
     }
 }
