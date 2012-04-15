249486135036f4f72d746f2503e4f9e01732acfb
Michel Bretschneider <embix@gmx.net>
2012-04-15 21:02:10 +0200

    Fix for It#2: set standard path to C:\

--- BiggestFileWinFormGui/Form1.cs
+++ BiggestFileWinFormGui/Form1.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using BiggestFiles;
 
@@ -12,9 +6,12 @@ namespace BiggestFileWinFormGui
 {
     public partial class BigFilesForm : Form
     {
+        private static readonly String StardardPath = @"C:\";
+
         public BigFilesForm()
         {
             InitializeComponent();
+            pathSelectionTextBox.Text = StardardPath;
         }
 
         private void searchButton_Click(object sender, EventArgs e)
