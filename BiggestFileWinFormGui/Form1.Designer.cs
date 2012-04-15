16d1b0182878e9a081d4f5d054f7a3bfa193cec0
Michel Bretschneider <embix@gmx.net>
2012-04-15 20:50:02 +0200

    2nd Iteration with Windows Forms GUI (synchronious, as requested for It#2

---
+++ BiggestFileWinFormGui/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace BiggestFileWinFormGui
+{
+    partial class BigFilesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pathSelectionTextBox = new System.Windows.Forms.TextBox();
+            this.pathSelectionButton = new System.Windows.Forms.Button();
+            this.biggestFilesListBox = new System.Windows.Forms.ListBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pathSelectionTextBox
+            // 
+            this.pathSelectionTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pathSelectionTextBox.Location = new System.Drawing.Point(13, 13);
+            this.pathSelectionTextBox.Name = "pathSelectionTextBox";
+            this.pathSelectionTextBox.Size = new System.Drawing.Size(437, 20);
+            this.pathSelectionTextBox.TabIndex = 0;
+            // 
+            // pathSelectionButton
+            // 
+            this.pathSelectionButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pathSelectionButton.Location = new System.Drawing.Point(456, 10);
+            this.pathSelectionButton.Name = "pathSelectionButton";
+            this.pathSelectionButton.Size = new System.Drawing.Size(75, 23);
+            this.pathSelectionButton.TabIndex = 1;
+            this.pathSelectionButton.Text = "select Path";
+            this.pathSelectionButton.UseVisualStyleBackColor = true;
+            // 
+            // biggestFilesListBox
+            // 
+            this.biggestFilesListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.biggestFilesListBox.FormattingEnabled = true;
+            this.biggestFilesListBox.Location = new System.Drawing.Point(13, 40);
+            this.biggestFilesListBox.Name = "biggestFilesListBox";
+            this.biggestFilesListBox.Size = new System.Drawing.Size(599, 316);
+            this.biggestFilesListBox.TabIndex = 2;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchButton.Location = new System.Drawing.Point(537, 10);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 3;
+            this.searchButton.Text = "search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // BigFilesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 362);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.biggestFilesListBox);
+            this.Controls.Add(this.pathSelectionButton);
+            this.Controls.Add(this.pathSelectionTextBox);
+            this.Name = "BigFilesForm";
+            this.Text = "Big Files";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox pathSelectionTextBox;
+        private System.Windows.Forms.Button pathSelectionButton;
+        private System.Windows.Forms.ListBox biggestFilesListBox;
+        private System.Windows.Forms.Button searchButton;
+    }
+}
+
---
+++ BiggestFileWinFormGui/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BiggestFiles;
+
+namespace BiggestFileWinFormGui
+{
+    public partial class BigFilesForm : Form
+    {
+        public BigFilesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            var startingWithPath = pathSelectionTextBox.Text;
+            var finder = new Finder(startingWithPath);
+            var biggestFiles = finder.FindRecursivly();
+            biggestFilesListBox.DataSource = biggestFiles;
+        }
+    }
+}
